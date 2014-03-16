98859cbac503c1062544439138ba5aa3da0c0f99
Marcelo Elizeche <melizeche@gmail.com>
2014-03-16 19:49:02 -0300

    url rewrite support, .htaccess

--- index.php
+++ index.php
@@ -5,9 +5,35 @@ require_once("controllers/configManager.php");
 require 'templateEngine.php';
 
 ## configuration ##
+
 $config =  new ConfigManager();
 $config->set('initialize','OK');
 $config::loadConfig();
+
+#urlrewrite
+$path = ltrim($_SERVER['REQUEST_URI'], '/');    // Trim leading slash(es)
+$elements = explode('/', $path);                // Split path on slashes
+var_dump($elements);
+if(count($elements) == 0)                       // No path elements means home
+    echo "realindex";
+else switch(array_shift($elements))             // Pop off first item and switch
+{
+    case '':
+        echo "index"; // passes rest of parameters to internal function
+        break;
+    case 'more':
+    	echo "more";
+    	break;
+    case 'post':
+    	echo "post";
+    	break;
+        
+    default:
+        header('HTTP/1.1 404 Not Found');
+       echo "Show404Error()";
+}
+
+
 //echo ConfigManager::get('xxx');
 //$cfg = ConfigManager::loadConfig(); 
 //$config::set($cfg);
--- views/posts.php
+++ views/posts.php
@@ -9,7 +9,7 @@ foreach ($posts as $post) {
 
 
       <a href='<?php echo "post.php?id=" . $post["_id"]  ?>' ><h2 class="post-title"><?php echo $post["titulo"]; ?></h2></a>
-      <p class="post-short"><?php  echo substr($post["text"], 0, 300); if(strlen($post["text"])>300) echo "...<b>(cont)</b>"; ?></p>
+      <p class="post-short"><?php  echo preg_replace("/[^\w\d ]/ui", '',substr($post["text"], 0, 300)); if(strlen($post["text"])>300) echo "...<b>(cont)</b>"; ?></p>
       <p class="post-meta"><?php  echo date("F j, Y, g:i a",$post["fecha"]) ?></p>
     </header>
   </section>
